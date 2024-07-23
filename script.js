964b70f1279db8b23e65d8d75c65c93fea528a15
Abhay Rana <anabhay42@gmail.com>
2024-07-23 17:04:19 +0530

    add bgm

--- script.js
+++ script.js
@@ -18,6 +18,29 @@ t1.from('.hero-image, .hero-info h2,.hero-info h3,.hero-info button,.hero-info i
 
 /* Coming Soon */
 
-function soon(){
+function soon() {
     alert("Yet to be Set");
 }
+
+/* Music */
+
+const music = document.getElementById('music');
+const audio = document.getElementById('audio');
+
+let flag = 0;
+audio.volume = 0.5;
+
+music.addEventListener('click', () => {
+    const fn = () => {
+        if (flag == 0) {
+            audio.play();
+            flag = 1;
+        }
+        else if (flag == 1) {
+            audio.pause();
+            audio.currentTime = 0;
+            flag = 0;
+        }
+    }
+    fn();
+})
