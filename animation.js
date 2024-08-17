f02f03b27cb2dcf8401552b7bc9d3b6f81a9d7f9
Abhay Rana <anabhay42@gmail.com>
2024-08-18 01:36:11 +0530

    animation-smooth

--- animation.js
+++ animation.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     let t1 = gsap.timeline();
     t1.to(".animate", { opacity: 1, visibility: "visible", duration: 1 });
-    t1.from('.logo, .nav-links li', {
+    t1.from('.logo, .nav-links li, .hamburger-image', {
         opacity: 0,
         y: -20,
         duration: 0.4,
         stagger: 0.2
     })
 
-    t1.from('.hero-image, .hero-info h2,.hero-info h3,.hero-info button,.hero-info img', {
+    t1.from('.hero-image, .hero-info h2,.hero-info h3,.hero-info button,.hero-info img, .music', {
         opacity: 0,
         y: 20,
         duration: .4,
