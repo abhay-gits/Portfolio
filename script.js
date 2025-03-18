7bc33f775b73602bb964d0941a6d098073886476
abhay-gits <anabhay42@gmail.com>
2025-03-18 18:20:53 +0530

    update

--- script.js
+++ script.js
@@ -3,26 +3,3 @@
 function soon() {
     alert("Yet to be Set");
 }
-
-/* Music */
-
-const music = document.getElementById('music');
-const audio = document.getElementById('audio');
-
-let flag = 0;
-audio.volume = 0.3;
-
-music.addEventListener('click', () => {
-    const fn = () => {
-        if (flag == 0) {
-            audio.play();
-            flag = 1;
-        }
-        else if (flag == 1) {
-            audio.pause();
-            audio.currentTime = 0;
-            flag = 0;
-        }
-    }
-    fn();
-})
